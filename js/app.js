035731e302beb072630c13f7cb7c2ed506a59910
MAZODA <oabumutlak@gmail.com>
2023-05-16 12:21:25 +0300

    Done

--- js/app.js
+++ js/app.js
@@ -32,11 +32,22 @@ function createArticles() {
 
   gridContainer.innerHTML = html;
 }
+createArticles();
 
 const popupButton = document.querySelector(".favorite-btn");
 const popupContainer = document.querySelector(".favorites");
-
 popupButton.addEventListener("click", function () {
   popupContainer.classList.toggle("active");
 });
-createArticles();
+const themeToggle = document.querySelector(".theme-toggle");
+
+function switchTheme() {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+
+  if (currentTheme === "dark") {
+    document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+}
+themeToggle.addEventListener("click", switchTheme, false);
